57076a9316d9625fcf1799524f08f08bdb7824bc
Matthieu Eck <matthieu.eck@gmail.com>
2018-05-04 16:51:53 +0200

    Update create-reverse.sh

--- jenkins/scripts/create-reverse.sh
+++ jenkins/scripts/create-reverse.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-ip=$(ifconfig  |  grep 'inet' | cut -d: -f1 | awk '{print $2}' | head -n 1)
+ip=$(ip a s |grep eth0| grep 'inet' | cut -d: -f1 | awk '{print $2}' | head -n 1 | cut -d'/' -f1)
 sed "s/@@@@FQDN@@@@/$1/g;s/@@@@IP@@@@/$ip/g;s/@@@@PORT@@@@/$2/g;" /var/jenkins_home/nginx/template.conf | tee /var/jenkins_home/nginx/reverse.d/$1
