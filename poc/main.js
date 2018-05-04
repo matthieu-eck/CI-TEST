f40ff67cff049071854d273e79bf049d794a5e31
Matthieu Eck <matthieu.eck@gmail.com>
2018-05-04 16:18:25 +0200

    Update main.js

--- poc/main.js
+++ poc/main.js
@@ -1,1 +1,9 @@
 
+var http = require('http');
+var os = require('os');
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  var a = os.hostname();
+  response.end("Hello World\n" + a);
+});
+server.listen(35000);
