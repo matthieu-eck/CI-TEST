354f0bc95e10c16d0ad623a1cdbc7e082e984b58
Matthieu Eck <matthieu.eck@gmail.com>
2018-05-04 16:18:38 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var os = require('os');
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  var a = os.hostname();
-  response.end("Hello World\n" + a);
-});
-server.listen(35000);
