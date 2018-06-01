3238bf6992a22e7a4f9ba3469b14dc0abe14ad97
Matthieu Eck <matthieu.eck@gmail.com>
2018-06-01 14:42:55 +0200

    Update create-reverse.sh

--- jenkins/scripts/create-reverse.sh
+++ jenkins/scripts/create-reverse.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 ip=$(ip a s |grep eth0| grep 'inet' | cut -d: -f1 | awk '{print $2}' | head -n 1 | cut -d'/' -f1)
-sed "s/@@@@FQDN@@@@/$1/g;s/@@@@IP@@@@/$ip/g;s/@@@@PORT@@@@/$2/g;" /var/jenkins_home/nginx/template.conf | tee /var/jenkins_home/nginx/reverse.d/$1
+container_hostname=$(hostname)
+sed "s/@@@@HOSTNAME_CONTAINER@@@@/$container_hostname/g;s/@@@@FQDN@@@@/$1/g;s/@@@@IP@@@@/$ip/g;s/@@@@PORT@@@@/$2/g;" /var/jenkins_home/nginx/template.conf | tee /var/jenkins_home/nginx/reverse.d/$1
